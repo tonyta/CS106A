4dca3893f11bd19e7b47f6270114cb0720a2074e
Tony Ta <tonyta.tt@gmail.com>
2013-11-08 19:59:12 -0800

    Add completed Assignment 2 - Problem 1

--- Assignment2/Pyramid.java
+++ Assignment2/Pyramid.java
@@ -1,13 +1,9 @@
 /*
  * File: Pyramid.java
- * Name: 
- * Section Leader: 
+ * Name: Tony Ta
  * ------------------
- * This file is the starter file for the Pyramid problem.
- * It includes definitions of the constants that match the
- * sample run in the assignment, but you should make sure
- * that changing these values causes the generated display
- * to change accordingly.
+ * The Pyramid class builds a pyramid using the constants for the
+ * brick dimensions and the number of bricks in the base.
  */
 
 import acm.graphics.*;
@@ -26,7 +22,24 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
-		/* You fill this in. */
+		int middleOfWindow = getWidth() / 2;
+		int bricksInRow = BRICKS_IN_BASE;
+		int brickYCoordinate = getHeight() - BRICK_HEIGHT;
+		//lays down each row and advances to the next
+		while (bricksInRow > 0) {
+			int halfRowLength = (BRICK_WIDTH * bricksInRow) / 2;
+			int brickXCoordinate = middleOfWindow - halfRowLength;
+			int bricksLeftInRow = bricksInRow;
+			//lays down each brick and advances to the next
+			while (bricksLeftInRow > 0) { //lays all bricks in current row
+				add(new GRect(brickXCoordinate, brickYCoordinate, BRICK_WIDTH, BRICK_HEIGHT));
+				brickXCoordinate += BRICK_WIDTH; //advances to next brick
+				bricksLeftInRow--; //reduces bricks to build by 1 in this row
+			}
+			brickYCoordinate -= BRICK_HEIGHT; //advances to next row
+			bricksInRow--; //reduces number of bricks by 1 for the next row
+		}
 	}
-}
 
+	
+}
