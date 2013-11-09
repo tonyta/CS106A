00523868b8af6e58a67145824ca1dacd63693539
Tony Ta <tonyta.tt@gmail.com>
2013-11-08 18:41:04 -0800

    Add completed Assignment 1 - Problem 3

--- Assignment1/CheckerboardKarel.java
+++ Assignment1/CheckerboardKarel.java
@@ -1,16 +1,84 @@
 /*
  * File: CheckerboardKarel.java
  * ----------------------------
- * When you finish writing it, the CheckerboardKarel class should draw
- * a checkerboard using beepers, as described in Assignment 1.  You
- * should make sure that your program works for all of the sample
- * worlds supplied in the starter folder.
+ * The CheckerboardKarel class draws a checkerboard of beepers in any
+ * sized world as described in Assignment 1 Problem 3.
  */
 
 import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
+	public void run () {
+		fillFirstRow();
+		while(leftIsClear()) {
+			fillEvenRow();
+			if (rightIsClear()) {
+				fillOddRow();
+			} else {
+				turnAround(); //Prevents Karel from looping
+			}
+		}
+	}
 
-	// You fill in this part
+/**
+ * Fills the first row with alternating beepers.
+ */
+	private void fillFirstRow() {
+		putBeeper();
+		putEveryOtherBeeper();		
+	}
+
+/**
+ * Fills a proceeding even row with alternating beepers.
+ */
+	private void fillEvenRow() {
+		if(beepersPresent()) {
+			turnLeft();
+			move();
+			turnLeft();
+		} else {
+			turnLeft();
+			move();
+			turnLeft();
+			putBeeper();
+		}
+		putEveryOtherBeeper();
+	}
+
+/**
+ * Fills a proceeding odd row with alternating beepers.
+ */
+	private void fillOddRow() {
+		if(beepersPresent()) {
+			turnRight();
+			move();
+			turnRight();
+		} else {
+			turnRight();
+			move();
+			turnRight();
+			putBeeper();
+		}
+		putEveryOtherBeeper();
+	}
+
+/**
+ * Puts a beeper on every other corner not including the one
+ * that Karel is currently standing on.
+ */
+	private void putEveryOtherBeeper() {
+		while (frontIsClear()) {
+			if (beepersPresent()) {
+				move();
+				if (frontIsClear()) {
+					move();
+					putBeeper();
+				}
+			} else {
+				move();
+				putBeeper();
+			}
+		}	
+	}
 
 }
