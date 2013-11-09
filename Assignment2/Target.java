59045ea5970a4f6806a2d73e2682dfb167ef17bc
Tony Ta <tonyta.tt@gmail.com>
2013-11-08 21:28:22 -0800

    Add completed Assignment 2 - Problem 2

--- Assignment2/Target.java
+++ Assignment2/Target.java
@@ -1,9 +1,9 @@
 /*
  * File: Target.java
- * Name: 
- * Section Leader: 
+ * Name: Tony Ta
  * -----------------
- * This file is the starter file for the Target problem.
+ * The Target Class creates a three ring, red-and-white target in
+ * the middle of the window.
  */
 
 import acm.graphics.*;
@@ -11,7 +11,26 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {	
+
+/** Number of pixels in an inch */
+	private static final int INCH = 72;
+		
 	public void run() {
-		/* You fill this in. */
+		add(circle(1.00*INCH, Color.red));
+		add(circle(0.65*INCH, Color.white));
+		add(circle(0.30*INCH, Color.red));
 	}
+
+/**
+ * This method returns a fill, colored circle of a specific radius.
+ */
+	private GOval circle(double r, Color color) {
+		int centerX = getWidth() / 2;
+		int centerY = getHeight() / 2;
+		GOval circle = new GOval(centerX-r, centerY-r, 2*r, 2*r);
+		circle.setFilled(true);
+		circle.setColor(color);
+		return circle;
+	}
+		
 }
