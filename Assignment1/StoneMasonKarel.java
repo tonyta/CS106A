3036665766412f45e5f71c8bc6bc5073ab5cda93
Tony Ta <tonyta.tt@gmail.com>
2013-11-08 18:38:37 -0800

    Add completed Assignment 1 - Problem 2

--- Assignment1/StoneMasonKarel.java
+++ Assignment1/StoneMasonKarel.java
@@ -1,17 +1,71 @@
 /*
  * File: StoneMasonKarel.java
  * --------------------------
- * The StoneMasonKarel subclass as it appears here does nothing.
- * When you finish writing it, it should solve the "repair the quad"
- * problem from Assignment 1.  In addition to editing the program,
- * you should be sure to edit this comment so that it no longer
- * indicates that the program does nothing.
+ * The StoneMasonKarel subclass repairs the columns of the Quad
+ * as described in Assignment 1 problem 2.
  */
 
 import stanford.karel.*;
 
 public class StoneMasonKarel extends SuperKarel {
+	public void run() {
+		while (frontIsClear()) {
+			repairCurrentColumn();
+			moveToNextColumn();
+		}
+		repairCurrentColumn();
+	}
+	
+/**
+ * Repair the current column.
+ * Precondition:	Karel is facing East at bottom of column.
+ * Postcondition:	Karel is facing East at bottom of column
+ * 					and the column is repaired.
+ */
+	private void repairCurrentColumn() {
+		turnLeft();
+		ascendToTopOfColumn();
+		turnAround();
+		repairGoingDownColumn();
+		turnLeft();
+	}
+	
+/**
+ * Ascend to the top of the column.
+ */
+	private void ascendToTopOfColumn() {
+		while (frontIsClear()) {
+			move();
+		}
+	}
+	
+/**
+ * Repair column on the way down.	
+ */
+	private void repairGoingDownColumn() {
+		while (frontIsClear()) {
+			repairCorner();
+			move();
+		}
+		repairCorner();
+	}
 
-	// You fill in this part
+/**
+ * Checks for beeper and puts one if none present.	
+ */
+	private void repairCorner() {
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+	}
+	
+/**
+ * Moves Karel to the next column.
+ */
+	private void moveToNextColumn() {
+		for (int i = 0; i < 4; i++) {
+			move();
+		}
+	}
 
 }
