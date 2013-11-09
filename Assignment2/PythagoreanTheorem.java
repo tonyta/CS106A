16d316d3ae95bdbecb9c9ec72b870542329e0ad5
Tony Ta <tonyta.tt@gmail.com>
2013-11-08 23:32:00 -0800

    Add completed Assignment 2 - Problem 4

--- Assignment2/PythagoreanTheorem.java
+++ Assignment2/PythagoreanTheorem.java
@@ -1,15 +1,20 @@
 /*
  * File: PythagoreanTheorem.java
- * Name: 
- * Section Leader: 
+ * Name: Tony Ta
  * -----------------------------
- * This file is the starter file for the PythagoreanTheorem problem.
+ * The PythagoreanTheorem calculates the lengths of the hypotenuse
+ * when given the other two sides of a triangle.
  */
 
 import acm.program.*;
 
+//Easy-mode... Engaged!
 public class PythagoreanTheorem extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		println("Enter values to compute Pythagorean theorem.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = Math.sqrt((a*a)+(b*b));
+		println("c = " + c );
 	}
 }
