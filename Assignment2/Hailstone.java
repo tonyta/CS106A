331afadd4478838348a25d5f4054b259bf319361
Tony Ta <tonyta.tt@gmail.com>
2013-11-09 01:02:47 -0800

    Add completed Assignment 2 - Problem 6

--- Assignment2/Hailstone.java
+++ Assignment2/Hailstone.java
@@ -1,16 +1,31 @@
 /*
  * File: Hailstone.java
- * Name: 
- * Section Leader: 
+ * Name: Tony Ta
  * --------------------
- * This file is the starter file for the Hailstone problem.
+ * The Hailstone class the "Hailstone sequence" for any non-negative integer.
  */
 
 import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		int processCount = 0;
+		int hailstone = readInt("Enter a number: ");
+		int hailstoneResult = hailstone;
+		while (hailstone <= 0) {	// prevents infinite loops and other weirdness.
+			hailstone = readInt("Cannot use zero or negatives. Try again: ");
+		}
+		while (hailstone != 1) {
+			if (hailstone % 2 == 0) {
+				hailstoneResult = hailstone/2;
+				println(hailstone + " is even so I take half: " + hailstoneResult);
+			} else {
+				hailstoneResult = 3*hailstone+1;
+				println(hailstone + " is odd, so I make 3n+1: " + hailstoneResult);
+			}
+			hailstone = hailstoneResult;
+			processCount++;
+		}
+		println("The process took " + processCount + " steps to reach 1.");
 	}
 }
-
