1cfcc46010fedbd1a50292ea772d3d3aa881aaa9
Tony Ta <tonyta.tt@gmail.com>
2013-11-16 08:40:02 -0800

    Clean up code and add comment.

--- ProblemSet2/Fibonacci.java
+++ ProblemSet2/Fibonacci.java
@@ -2,8 +2,8 @@
  * File: Fibonacci.java
  * Name: Tony Ta
  * --------------------
- * The Fibonacci class prints the Fibonacci sequence until the constant
- * MAX_VALUE_TERM is reached.
+ * The Fibonacci class prints the Fibonacci sequence for results less than
+ * the constant MAX_VALUE_TERM.
  */
 
 import acm.program.*;
@@ -13,17 +13,16 @@ public class Fibonacci extends ConsoleProgram {
 	public void run() {
 		println("This program lists the Fibonacci sequence.");
 		int term = 0;
-		int term1Ago = 1;
-		int term2Ago = 0;
+		int term1Ago = 1; //initialized at 1 to force Fib(1) = 1
 		while (term < MAX_VALUE_TERM) {
 			println(term);
-			term2Ago = term1Ago;
+			int term2Ago = term1Ago;
 			term1Ago = term;
 			term = term1Ago + term2Ago;
 		}
-		
 	}
 	
+/* Sequence will print for results that are less than this max. */
 	private static final int MAX_VALUE_TERM = 10000;
 	
 }
